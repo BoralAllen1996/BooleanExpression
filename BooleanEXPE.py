84a964e78a7969e078e6050defdfb79a4b2d5d1c
lenbo <allenbora@gmail.com>
2022-12-23 17:42:23 +0800

    fourteenth

---
+++ BooleanEXPE.py
@@ -0,0 +1,14 @@
+# To identify the true or false on the given number , I used and assigned print statement for the results.
+
+#True
+statement_one = (5 * 2) - 1 == 8 + 1
+print(statement_one)
+
+#False
+statement_two = 13 - 6 != (3 * 2) + 1
+print(statement_two)
+
+#True
+statement_three = 3 * (2 - 1) == 4 - 1
+print(statement_three)
+#end.....
